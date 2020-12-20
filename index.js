8c8c9f75f53827885de28a93c629b286abf46908
David Maier <david.maier@atempo.at>
2020-12-20 22:13:40 +0100

    changed result

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.send('Get me started!!');
 })
 
 app.listen(3000, function () {
-  console.log('Starting hello-world server...');
+  console.log('Get me started!');
 })
